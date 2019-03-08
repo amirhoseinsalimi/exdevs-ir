d91c1a85f424ac47216a24c8926e0c4809be648b
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2019-03-08 13:18:08 +0330

    Add 404
    Add 404 page from https://codepen.io/diogo_ml_gomes/pen/PyWdLb

--- app.js
+++ app.js
@@ -27,7 +27,8 @@ app.use('/contact-message', contactMessageRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  // next(createError(404));
+    res.render('404');
 });
 
 // error handler
