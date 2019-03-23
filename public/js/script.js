f82773f0c9d336973f02221eb68f288c5d8560b3
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2019-03-23 14:31:54 +0430

    Fix sentences w/ Grammerly

--- public/js/script.js
+++ public/js/script.js
@@ -17,13 +17,13 @@ $(() => {
   const teamsInfo = [
     {
       name: 'Web development',
-      description: 'Web is an essential part of every successful product nowadays. Our web'
-      + ' division, having enthusiastic developers, provides you every website and web'
-      + ' application you want with a exemplary quality. We use high-demand frameworks and latest'
-      + ' technologies as our main tool to bring this achievement. Our speciality in JavaScript'
-      + ' front-end and back-end frameworks, gives us the ability to build robust full-stack web'
-      + ' apps with an astonishing performance. Long story short, we do our best to always be on'
-      + ' the edge of web development world.',
+      description: 'The web is an essential part of every successful product nowadays. Our web'
+      + ' division, having enthusiastic developers, provides you each website and web application'
+      + '  you want with exemplary quality. We use high-demand frameworks and the latest'
+      + ' technologies as our main to ol to bring this achievement. Our specialty in front-end and'
+      + '  back-end JavaScript frameworks gives us the ability to build robust full-stack web apps'
+      + '  with an astonishing performance. Long story short, we do our best to always be on the'
+      + '  edge of the web development world.',
       motto: 'Always deliver more than expected',
       color: '#f0db4f',
       icon: 'thumbs/web.svg',
@@ -32,11 +32,11 @@ $(() => {
       name: 'Application development',
       description: 'Want to expand your business? Maybe you need a mobile app! Our experienced'
       + ' developers at our mobile division, are capable of doing anything possible in the'
-      + ' industry. Having the experience in Java, Kotlin and Flutter, there\'s no goddamn'
-      + ' obstacle for us to go beyond the boundaries. We build your desired Android or iOS app'
-      + ' with the quality, fewer young teams would be capable of. With the knowledge of a bunch'
-      + ' of technologies, we always choose the right one for the job, and it wouldn\'t be'
-      + ' exaggerating to say that nothing is impossible for us.',
+      + ' industry. Having experience in Java, Kotlin, Flutter, there\'s no obstacle for us to go'
+      + ' beyond the boundaries. We build your desired Android or iOS app with the quality, fewer'
+      + ' young teams would be capable. With the knowledge of a bunch of technologies, we always'
+      + ' choose the right one for the job, and it wouldn\'t be exaggerating to say that nothing is'
+      + ' impossible for us.',
       motto: 'Success is not a final point, it\'s a road',
       color: '#4ff05f',
       icon: 'thumbs/app.svg',
@@ -50,14 +50,14 @@ $(() => {
     },
     {
       name: 'Server development',
-      description: 'The spirit of every mobile app, website or a web service is its back-end.'
-      + ' PHP, Elixir and Python are just a few tools we have in our hands, but our real power'
-      + ' doesn\'t come from these tools, it comes from our mind! We know all the feasible features'
-      + ' your product needs, so you can trust us on all kinds of projects,'
-      + ' whether it is a RESTful API or a back-end for a website, we do the correct'
-      + ' job. We\'ve also come a looong way in designing databases and MySQL/MariaDB,'
-      + ' MongoDB were our best friends during our journey. We know that success doesn\'t'
-      + ' achieved by accident, so we don\'t wait for it, we chase it!.',
+      description: 'The spirit of every mobile app, website or a web service is server-side.'
+      + ' PHP, Elixir, Python are just a few tools we have in our hands, but our real power'
+      + ' doesn\'t come from these tools, it comes from our mind! We know all the feasible'
+      + ' features your product needs, so you can trust us on all kinds of projects, whether it is'
+      + ' a RESTful API or a back-end for a website, we do the correct job. We\'ve also come a'
+      + ' long way in designing databases and MySQL/MariaDB, MongoDB were our best friends during'
+      + ' our journey. We know that success isn\'t achieved by accident, so we don\'t wait for it,'
+      + ' we chase it!',
       motto: 'Use the right tool, for the right job, in the right way!',
       color: '#4f8af0',
       icon: 'thumbs/server.svg',
@@ -88,8 +88,8 @@ $(() => {
     currentProgress += 1;
 
     $('#dynamic')
-      .css('width', `${currentProgress}%`)
-      .attr('aria-valuenow', currentProgress);
+        .css('width', `${currentProgress}%`)
+        .attr('aria-valuenow', currentProgress);
 
     if (currentProgress >= 106) {
       changeTeam(++currentTeam >= 4 ? currentTeam = 0 : currentTeam);
