42bbc60af543d550c3cbf0d1affc7001376eecab
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2019-04-14 12:37:33 +0430

    Uncomment connection file

--- routes/contact-message.js
+++ routes/contact-message.js
@@ -1,33 +1,33 @@
 const express = require('express');
 
 const router = express.Router();
-// const connection = require('../connection');
+const connection = require('../connection');
 
 /* Process POST data */
-// router.post('/', (req, res) => {
-//   connection.connect((err) => {
-//     if (err) {
-//       res.status(500);
-//       res.render('500');
-//     } else {
-//       const { name } = req.body;
-//       const { email } = req.body;
-//       const { message } = req.body;
+router.post('/', (req, res) => {
+  connection.connect((err) => {
+    if (err) {
+      res.status(500);
+      res.render('500');
+    } else {
+      const { name } = req.body;
+      const { email } = req.body;
+      const { message } = req.body;
 
-//       const query = 'INSERT INTO messages (name, email, text) VALUES (?, ?, ?);';
+      const query = 'INSERT INTO messages (name, email, text) VALUES (?, ?, ?);';
 
-//       connection.query(query, [name, email, message], (err) => {
-//         if (err) {
-//           res.status(500);
-//           res.render('500');
-//         } else {
-//           res.redirect('/');
-//         }
+      connection.query(query, [name, email, message], (err) => {
+        if (err) {
+          res.status(500);
+          res.render('500');
+        } else {
+          res.redirect('/');
+        }
 
-//         connection.end();
-//       });
-//     }
-//   });
-// });
+        connection.end();
+      });
+    }
+  });
+});
 
 module.exports = router;
