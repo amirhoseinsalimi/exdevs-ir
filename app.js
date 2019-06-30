67125fcfe4eb08a97b8dedee237adc2463e0c3f9
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2019-06-30 15:23:36 +0430

    Install helmet

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 
 // Routers
 const rootRouter = require('./routes/index');
@@ -15,6 +16,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(helmet());
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
