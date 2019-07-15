c5722244510b58190380bb1fc4e1016864416cd6
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2019-07-15 22:24:51 +0430

    ESLint

--- routes/downloader.js
+++ routes/downloader.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.redirect(`https://cafebazaar.ir/app/com.digikala/?l=en`);
+router.get('/', (req, res) => {
+  res.redirect('https://cafebazaar.ir/app/com.digikala/?l=en');
 });
 
 module.exports = router;
