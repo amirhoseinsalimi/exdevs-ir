a3688352b1c7a0b3bf71831f69f23cb9f09acbd4
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2019-07-18 01:01:36 +0430

    Install gulp and integrate the project with it

---
+++ gulpfile.js
@@ -0,0 +1,96 @@
+// Gulp.js configuration
+
+/* Development mode? */
+const devBuild = (process.env.NODE_ENV !== 'production');
+
+/* Folders */
+const src = 'src/';
+const dest = 'public/';
+
+/* Modules */
+const gulp = require('gulp');
+const newer = require('gulp-newer');
+const imagemin = require('gulp-imagemin');
+const noop = require('gulp-noop');
+const concat = require('gulp-concat');
+const deporder = require('gulp-deporder');
+const terser = require('gulp-terser');
+const stripdebug = devBuild ? null : require('gulp-strip-debug');
+const sourcemaps = devBuild ? require('gulp-sourcemaps') : null;
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const assets = require('postcss-assets');
+const autoprefixer = require('autoprefixer');
+const mqpacker = require('css-mqpacker');
+const cssnano = require('cssnano');
+
+
+/* Module configuration */
+sass.compiler = require('node-sass');
+
+
+/* Tasks configuration */
+
+// Image processing
+function images() {
+  const out = `${dest}/img`;
+
+  return gulp.src(`${src}images/**/*`)
+    .pipe(newer(out))
+    .pipe(imagemin({ optimizationLevel: 5 }))
+    .pipe(gulp.dest(out));
+}
+
+// JS processing
+function js() {
+  return gulp.src(`${src}js/**/*`)
+    .pipe(sourcemaps ? sourcemaps.init() : noop())
+    .pipe(deporder())
+    .pipe(concat('main.js'))
+    .pipe(stripdebug ? stripdebug() : noop())
+    .pipe(terser())
+    .pipe(sourcemaps ? sourcemaps.write() : noop())
+    .pipe(gulp.dest(`${dest}js/`));
+}
+
+// Sass and CSS processing
+function css() {
+  return gulp.src(`${src}scss/main.scss`)
+    .pipe(sourcemaps ? sourcemaps.init() : noop())
+    .pipe(sass({
+      outputStyle: 'nested',
+      imagePath: '/images/',
+      precision: 3,
+      errLogToConsole: true,
+    }).on('error', sass.logError))
+    .pipe(postcss([
+      assets({ loadPaths: ['images/'] }),
+      autoprefixer({ browsers: ['last 2 versions', '> 2%'] }),
+      mqpacker,
+      cssnano,
+    ]))
+    .pipe(sourcemaps ? sourcemaps.write() : noop())
+    .pipe(gulp.dest(`${dest}css/`));
+}
+
+// Watch for file changes
+function watch(done) {
+  // image changes
+  gulp.watch(`${src}images/**/*`, images);
+
+  // js changes
+  gulp.watch(`${src}js/**/*`, js);
+
+  // css changes
+  gulp.watch(`${src}scss/**/*`, css);
+
+  done();
+}
+
+
+/* Tasks */
+exports.images = images;
+exports.js = js;
+exports.css = css;
+exports.build = gulp.parallel(exports.images, exports.css, exports.js); // run all tasks
+exports.watch = watch;
