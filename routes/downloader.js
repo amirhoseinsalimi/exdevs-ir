20d4e848ebf9766e237c32a87ff68d4a0726f83f
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2019-07-18 09:29:29 +0430

    Direct download link

--- routes/downloader.js
+++ routes/downloader.js
@@ -3,7 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.redirect('https://cafebazaar.ir/app/ir.expteam.timeset/');
+  // res.redirect('https://cafebazaar.ir/app/ir.expteam.timeset/');
+  res.download('app.apk');
 });
 
 module.exports = router;
