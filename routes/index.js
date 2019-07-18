29696dd8e9ebca8d670b9ba93c0522f04fd4dd21
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2019-07-19 00:38:44 +0430

    ESLint

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-let express = require('express'),
-    router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
   res.render('index', { title: 'Express' });
 });
 
