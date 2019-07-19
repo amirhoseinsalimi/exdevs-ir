055941bed02c90c45b459eed0e5461f59e39c1e3
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2019-07-19 13:57:28 +0430

    Set MIME type for download route

--- routes/downloader.js
+++ routes/downloader.js
@@ -6,6 +6,7 @@ const { incrementDownloadLinkCounter } = require('../counter');
 
 router.get('/', (req, res) => {
   // res.redirect('https://cafebazaar.ir/app/ir.expteam.timeset/');
+  res.set({ 'Content-Type': 'application/vnd.android.package-archive' });
   res.download('app.apk');
 
   incrementDownloadLinkCounter();
