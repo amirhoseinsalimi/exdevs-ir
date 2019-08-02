a0bf2e45eca7b94e8e7d1c7b1ea6051eec50e38f
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2019-08-02 22:19:33 +0430

    Delete previous Gulp configuration and re-configure it (Only CSS configuration at the moment)
    Create "style" task which autoprefixes, minifes, renames and compiles SCSS to CSS and generates source maps.

--- gulpfile.js
+++ gulpfile.js
@@ -1,98 +1,27 @@
-// Gulp.js configuration
-
-/* Development mode? */
-const devBuild = (process.env.NODE_ENV !== 'production');
-
-
-/* Folders */
-const src = 'src/';
-const dest = 'public/';
-
-
-/* Modules */
 const gulp = require('gulp');
-const newer = require('gulp-newer');
-const imagemin = require('gulp-imagemin');
-const noop = require('gulp-noop');
-const concat = require('gulp-concat');
-const deporder = require('gulp-deporder');
-const terser = require('gulp-terser');
-const stripdebug = devBuild ? null : require('gulp-strip-debug');
-const sourcemaps = devBuild ? require('gulp-sourcemaps') : null;
+
+/* Modules related SCSS and CSS */
+const rename = require('gulp-rename');
 const sass = require('gulp-sass');
-const postcss = require('gulp-postcss');
-const assets = require('postcss-assets');
-const autoprefixer = require('autoprefixer');
-const mqpacker = require('css-mqpacker');
-const cssnano = require('cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
 
 
-/* Module configuration */
+/* CSS processing */
 sass.compiler = require('node-sass');
 
+const srcPath = './src/scss/**/*.scss';
+const distPath = './public/css/';
 
-/* Tasks configuration */
-
-// Image processing
-function images() {
-  const out = `${dest}/img`;
-
-  return gulp.src(`${src}img/**/*`)
-    .pipe(newer(out))
-    .pipe(imagemin({ optimizationLevel: 5 }))
-    .pipe(gulp.dest(out));
-}
-
-// JS processing
-function js() {
-  return gulp.src(`${src}js/**/*`)
-    .pipe(sourcemaps ? sourcemaps.init() : noop())
-    .pipe(deporder())
-    .pipe(concat('main.js'))
-    .pipe(stripdebug ? stripdebug() : noop())
-    .pipe(terser())
-    .pipe(sourcemaps ? sourcemaps.write() : noop())
-    .pipe(gulp.dest(`${dest}js/`));
-}
-
-// Sass and CSS processing
-function css() {
-  return gulp.src(`${src}scss/main.scss`)
-    .pipe(sourcemaps ? sourcemaps.init() : noop())
-    .pipe(sass({
-      outputStyle: 'nested',
-      imagePath: '/img/',
-      precision: 3,
-      errLogToConsole: true,
-    }).on('error', sass.logError))
-    .pipe(postcss([
-      assets({ loadPaths: ['img/'] }),
-      autoprefixer({ browsers: ['last 2 versions', '> 2%'] }),
-      mqpacker,
-      cssnano,
-    ]))
-    .pipe(sourcemaps ? sourcemaps.write() : noop())
-    .pipe(gulp.dest(`${dest}css/`));
-}
-
-// Watch for file changes
-function watch(done) {
-  // image changes
-  gulp.watch(`${src}img/**/*`, images);
-
-  // js changes
-  gulp.watch(`${src}js/**/*`, js);
-
-  // css changes
-  gulp.watch(`${src}scss/**/*`, css);
-
-  done();
-}
-
-
-/* Tasks */
-exports.images = images;
-exports.js = js;
-exports.css = css;
-exports.build = gulp.parallel(exports.images, exports.css, exports.js); // run all tasks
-exports.watch = watch;
+gulp.task('style', () => gulp.src(srcPath)
+  .pipe(sourcemaps.init())
+  .pipe(sass({
+    outputStyle: 'compressed',
+  })
+    .on('error', sass.logError))
+  .pipe(autoprefixer({
+    cascade: false,
+  }))
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest(distPath)));
