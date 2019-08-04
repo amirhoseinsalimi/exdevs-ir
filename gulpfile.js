30c389939515d9b97d820ff96bf57e010b5fe161
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2019-08-05 02:08:00 +0430

    Setup browser-sync to hot reload changes
    Install browser-sync and gulp-nodemon. Setup separate task for nodemon and browser-sync and integrate them w/ each other. Also add a task to serve and build project. Move Default task to the bottom of the file.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const gulp = require('gulp');
 
 
 /* General plugins */
+const browserSync = require('browser-sync').create();
+const nodemon = require('gulp-nodemon');
 const sourcemaps = require('gulp-sourcemaps');
 
 
@@ -22,7 +24,6 @@ const noop = require('gulp-noop');
 /* Paths */
 const styleSrc = 'src/scss/**/*.scss';
 const styleDest = './public/css/';
-
 const scriptSrc = 'src/js/**/*.js';
 const scriptDest = './public/js/';
 
@@ -31,7 +32,7 @@ const scriptDest = './public/js/';
 sass.compiler = require('node-sass');
 
 
-/* SCSS to CSS */
+/* SCSS to CSS task */
 gulp.task('style', () => gulp.src(styleSrc)
   .pipe(sourcemaps.init())
   .pipe(sass({
@@ -57,8 +58,45 @@ gulp.task('script', () => gulp.src(scriptSrc)
   .pipe(gulp.dest(scriptDest)));
 
 
-/* Default task */
-gulp.task('default', gulp.series('style', 'script'));
+/* Nodemon task */
+gulp.task('nodemon', (done) => {
+  const STARTUP_TIMEOUT = 5000;
+  const server = nodemon({
+    script: 'bin/www',
+    stdout: false, // without this line the stdout event won't fire
+  });
+  let starting = false;
+
+  const onReady = () => {
+    starting = false;
+    done();
+  };
+
+  server.on('start', () => {
+    starting = true;
+    setTimeout(onReady, STARTUP_TIMEOUT);
+  });
+
+  server.on('stdout', (stdout) => {
+    process.stdout.write(stdout); // pass the stdout through
+    if (starting) {
+      onReady();
+    }
+  });
+});
+
+
+/* Browser-Sync Task */
+gulp.task('browser-sync', (done) => {
+  browserSync.init({
+    proxy: 'http://localhost:3001',
+    files: ['public/**/*.*', 'views/**/*.*', 'src/**/*.*', '*.js', 'routes/*.js', 'bin/www'],
+    port: 3000,
+    ui: {
+      port: 3030,
+    },
+  }, done);
+});
 
 
 /* Watch files */
@@ -68,3 +106,15 @@ gulp.task('watch', (done) => {
 
   done();
 });
+
+
+/* Serve project */
+gulp.task('serve', gulp.series('nodemon', 'browser-sync'));
+
+
+/* Build project */
+gulp.task('build', gulp.series('style', 'script'));
+
+
+/* Default task */
+gulp.task('default', gulp.series('serve', 'watch'));
