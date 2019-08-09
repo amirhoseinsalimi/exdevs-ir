b2cfcc6903c18d09a892edd410f55a2fea7d8583
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2019-08-09 19:23:14 +0430

    Add Shahryar Hajian's profile

--- routes/get-content.js
+++ routes/get-content.js
@@ -128,6 +128,16 @@ router.get('/', (req, res) => {
       email: 'a.geramian@yahoo.com',
       telegram: 'Ali_GR',
     },
+    {
+      name: 'Shahryar Hajian',
+      position: 'Researcher',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      img: 'img/hajian.jpg',
+      linkedIn: 'shahryar-hajian-209a50151/',
+      twitter: '',
+      email: 'Hajian.rh@gmail.com',
+      telegram: 'Shaha_H',
+    },
   ];
 
   const result = {
