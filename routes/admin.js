eba10a5319720c9b6d98ab4268fb654d1cf3cc3d
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2019-08-12 15:05:34 +0430

    Fix request method

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
-const connection = require('../connection');
 
-router.post('/', (req, res) => {
+router.get('/', (req, res) => {
+  res.render('admin/index');
 });
 
 module.exports = router;
