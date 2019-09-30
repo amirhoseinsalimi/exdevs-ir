8263495796c02bd27a09aa01fbfca986e82f13df
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2019-09-30 10:23:54 +0330

    Add a key to hash cookies

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cookieParser());
+app.use(cookieParser('thisissecret'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', rootRouter);
