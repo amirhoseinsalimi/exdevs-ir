37969be89596763a6098903d1e717ad5ef87703d
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2019-10-01 12:41:20 +0330

    Fix seeds to fit the new `message` table

--- db/migrations/sqls/20190813105926-message-up.sql
+++ db/migrations/sqls/20190813105926-message-up.sql
@@ -12,15 +12,15 @@ CREATE TABLE IF NOT EXISTS  `message` (
 );
 
 /* Seed table */
-INSERT INTO `message` (sender_name, sender_email, message_text, is_read, date, time) VALUES
-    ('Amir Hosein', 'ahosein.salimi@gmail.com', 'This is a message', 0, '2001-10-10', '09:09:09'),
-    ('Amir Mohammad', 'amohammad.salimi@gmail.com', 'This is a message', 0, '2019-01-10', '10:10:10'),
-    ('Fatemeh', 'ftad3v@gmail.com', 'This is a message', 1, '1999-10-10', '11:11:11'),
-    ('Saeed', 'saeedev@gmail.com', 'This is a message', 0, '1999-10-10', '12:12:12'),
-    ('Milad', 'mikdev@gmail.com', 'This is a message', 1, '1999-10-10', '13:13:13'),
-    ('Hamid', 'hrbdev@gmail.com', 'This is a message', 0, '1999-10-10', '14:14:14'),
-    ('Mahboubeh', 'mahidev@gmail.com', 'This is a message', 0, '1999-10-10', '15:15:15'),
-    ('Amir', 'amiagr@gmail.com', 'This is a message', 0, '1999-10-10', '15:15:15'),
-    ('Marzieh', 'marzidev@gmail.com', 'This is a message', 1, '1999-10-10', '15:15:15'),
-    ('Ali', 'aliux@gmail.com', 'This is a message', 1, '1999-10-10', '15:15:15'),
-    ('Mohammad Amin', 'danshoor@gmail.com', 'This is a message', 0, '1999-10-10', '15:15:15');
+INSERT INTO `message` (sender_name, sender_email, message_text, is_read) VALUES
+    ('Amir Hosein', 'ahosein.salimi@gmail.com', 'This is a message', 0),
+    ('Amir Mohammad', 'amohammad.salimi@gmail.com', 'This is a message', 0),
+    ('Fatemeh', 'ftad3v@gmail.com', 'This is a message', 1),
+    ('Saeed', 'saeedev@gmail.com', 'This is a message', 0),
+    ('Milad', 'mikdev@gmail.com', 'This is a message', 1),
+    ('Hamid', 'hrbdev@gmail.com', 'This is a message', 0),
+    ('Mahboubeh', 'mahidev@gmail.com', 'This is a message', 0),
+    ('Amir', 'amiagr@gmail.com', 'This is a message', 0),
+    ('Marzieh', 'marzidev@gmail.com', 'This is a message', 1),
+    ('Ali', 'aliux@gmail.com', 'This is a message', 1),
+    ('Mohammad Amin', 'danshoor@gmail.com', 'This is a message', 0);
