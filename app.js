651b3102717d0c8e21644c6504c7ca545db74801
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2019-10-01 10:23:13 +0330

    Change secret key for hashing cookies

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cookieParser('thisissecret'));
+app.use(cookieParser('bethesmartestpersomintheroot'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', rootRouter);
