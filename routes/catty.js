eb3fb772f93074331f776a32c2a29e9b882ae351
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2019-10-12 12:04:31 +0330

    Check password against the hashed variant using `bcrypt`

--- routes/catty.js
+++ routes/catty.js
@@ -1,7 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
+const bcrypt = require('bcrypt');
 const pool = require('../connection');
+
+const saltRounds = 10;
 
 /* GET admin page. */
 router.get('/', (req, res) => {
@@ -22,28 +25,29 @@ router.post('/', (req, res) => {
       const { user } = req.body;
       const { password } = req.body;
 
-      const query = 'SELECT * FROM admin WHERE username=? AND password=?;';
+      const query = 'SELECT * FROM admin WHERE username=?';
 
-      connection.query(query, [user, password], (err, result) => {
+      connection.query(query, [user], (err, results) => {
         if (err) {
           console.log(`Error executing the query: ${err.name}: ${err.message}`);
           res.status(500);
           res.render('500');
-        } else if (result.length > 0) {
-          if (err) {
-            console.log(`Error executing the query: ${err.name}: ${err.message}`);
-            res.status(500);
-            res.render('500');
-          } else {
-            const date = (new Date(Date.now() + 86400 * 1000)).toUTCString();
-            res.cookie('superuser', 'yes,heis', {
-              expires: date * 7 * 86400,
-              path: '/',
-              signed: true,
-              secure: true,
-            });
-            res.redirect('/yttac');
-          }
+        } else if (results.length > 0) {
+          bcrypt.compare(password, results[0].password).then((matched) => {
+            if (matched) {
+              const date = (new Date(Date.now() + 86400 * 1000)).toUTCString();
+              res.cookie('superuser', 'yes,heis', {
+                expires: date * 7 * 86400,
+                path: '/',
+                signed: true,
+                secure: true,
+              });
+              res.redirect('/yttac');
+            } else {
+              res.status(401);
+              res.render('catty');
+            }
+          });
         } else {
           res.status(401);
           res.render('catty');
