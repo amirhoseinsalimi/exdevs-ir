0cf79f079a8e0db8186408a1c63554e68208dfb3
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2020-04-04 17:33:06 +0430

    Exclude `connection.js` from .gitignore

---
+++ connection.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+const {
+  DB_HOST: host,
+  DB_PORT: port,
+  DB_NAME: database,
+  DB_USER: user,
+  DB_PASSWORD: password,
+} = require('./config');
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  port,
+  host,
+  user,
+  password,
+  database,
+});
+
+module.exports = pool;
