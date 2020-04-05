65b6768c5acd35f7b184a3417a729a5a04ebeba6
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2020-04-06 01:59:15 +0430

    Add knexfile.js

---
+++ knexfile.js
@@ -0,0 +1,62 @@
+/* Knex.js configuration
+   See http://knexjs.org/ for documents
+* */
+const {
+  DB_NAME: database,
+  DB_USERNAME: user,
+  DB_PASSWORD: password,
+} = require('./config');
+
+module.exports = {
+  development: {
+    client: 'mysql', // Your database driver
+    connection: {
+      database,
+      user,
+      password,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/database/migrations`,
+    },
+    seeds: {
+      directory: `${__dirname}/database/seeds`,
+    },
+  },
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+};
