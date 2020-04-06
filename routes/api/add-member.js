47beabede02e95776d7c6504949570bbba86e106
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2020-04-06 22:45:57 +0430

    Use multer to handle file uploads and knex.js to query the database

--- routes/api/add-member.js
+++ routes/api/add-member.js
@@ -1,66 +1,24 @@
 const express = require('express');
 
 const router = express.Router();
-const pool = require('../../connection');
+const multer = require('multer');
+const connection = require('../../connection');
+const knex = require('knex')(connection); // eslint-disable-line
 
-router.post('/', (req, res) => {
-  let {
-    firstName,
-    lastName,
-    team,
-    position,
-    briefText,
-    github,
-    telegram,
-    twitter,
-    email,
-    linkedIn,
-  } = req.body;
+const upload = multer({ dest: 'uploads/' });
 
-  firstName = firstName || '';
-  lastName = lastName || '';
-  team = team || '';
-  position = position || '';
-  briefText = briefText || '';
-  github = github || '';
-  telegram = telegram || '';
-  email = email || '';
-  linkedIn = linkedIn || '';
-  twitter = twitter || '';
+router.post('/', upload.single('photo'), (req, res) => {
+  const requestData = req.body;
+  requestData.photo = req.file.filename;
 
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.log(`Error connecting to database: ${err.name}: ${err.message}`);
-      res.status(500);
-      res.render('500');
-    }
-
-    const query = 'INSERT INTO ex_website.member (first_name, last_name, team, position, brief_text, github, linkedin, twitter, telegram, email) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-
-    connection.query(query, [
-      firstName,
-      lastName,
-      team,
-      position,
-      briefText,
-      github,
-      linkedIn,
-      twitter,
-      telegram,
-      email,
-    ], (err) => {
-      if (err) {
-        console.log(err);
-        res.status(500);
-        res.render('500');
-      } else {
-        res.status(204);
-        res.end();
-      }
-
-      connection.release();
+  knex('users')
+    .insert(requestData)
+    .then(() => {
+      // Handle later
+    })
+    .catch((err) => {
+      throw Error(err);
     });
-  });
 });
 
 module.exports = router;
