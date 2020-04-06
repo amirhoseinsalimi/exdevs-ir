fdc0c1f23d51d6eb810be39b6ee9fa73352f8938
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2020-04-07 02:40:05 +0430

    Catch promises to prevent getting errors on `down` and `rollback`

--- database/migrations/20200406231305_create_teams_table.js
+++ database/migrations/20200406231305_create_teams_table.js
@@ -1,20 +1,28 @@
-exports.up = (knex) => {
-  knex.schema.hasTable('teams').then((exists) => {
-    if (!exists) {
-      return knex.schema.createTable('teams', (table) => {
-        table.increments().primary();
-        table.string('name').notNullable();
-        table.text('description').notNullable();
-        table.timestamps();
-      });
-    }
-  });
+exports.up = async (knex) => {
+  knex.schema
+    .hasTable('teams')
+    .then((exists) => {
+      if (!exists) {
+        return knex.schema.createTable('teams', (table) => {
+          table.increments().primary();
+          table.string('name').notNullable();
+          table.text('description').notNullable();
+          table.timestamps(true, true);
+        });
+      }
+    })
+    .catch(() => {});
 };
 
-exports.down = (knex) => {
-  knex.schema.hasTable('teams').then((exists) => {
-    if (exists) {
-      return knex.schema.dropTableIfExists('teams');
-    }
-  });
+exports.down = async (knex) => {
+  knex.schema
+    .hasTable('teams')
+    .then((exists) => {
+      if (exists) {
+        return knex.schema.dropTableIfExists('teams');
+      }
+    })
+    .catch(() => {});
 };
+
+exports.config = { transaction: false };
