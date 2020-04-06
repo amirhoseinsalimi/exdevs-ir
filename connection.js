657efc3ffa4aa5e60f4bba3208d39277e1523437
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2020-04-06 22:44:54 +0430

    Export the raw connection object instead of creating a pool

--- connection.js
+++ connection.js
@@ -1,19 +1,18 @@
-const mysql = require('mysql');
 const {
   DB_HOST: host,
-  DB_PORT: port,
   DB_NAME: database,
   DB_USERNAME: user,
   DB_PASSWORD: password,
 } = require('./config');
 
-const pool = mysql.createPool({
-  connectionLimit: 10,
-  port,
-  host,
-  user,
-  password,
-  database,
-});
+const connection = {
+  client: 'mysql',
+  connection: {
+    host,
+    database,
+    user,
+    password,
+  },
+};
 
-module.exports = pool;
+module.exports = connection;
