e04bf89214740b181646d73644f4a5be95a16e11
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2020-04-07 10:32:33 +0430

    Populate knex.js production config with .env vars.

--- knexfile.js
+++ knexfile.js
@@ -44,19 +44,19 @@ module.exports = {
   //   }
   // },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+  production: {
+    client: 'postgresql',
+    connection: {
+      database,
+      user,
+      password,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
 };
