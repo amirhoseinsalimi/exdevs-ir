fdca4ddd290fa21fb03ec7a14f837f37c66bf74e
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2020-04-07 09:40:24 +0430

    Use knex.js to store incoming messages

--- routes/contact-message.js
+++ routes/contact-message.js
@@ -1,34 +1,20 @@
 const express = require('express');
 
 const router = express.Router();
-const pool = require('../connection');
+const connection = require('../connection');
+const knex = require('knex')(connection); // eslint-disable-line
 
 /* Process POST data */
 router.post('/', (req, res) => {
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.log(err);
-      res.status(500);
-      res.render('500');
-    } else {
-      const { name } = req.body;
-      const { email } = req.body;
-      const { message } = req.body;
-
-      const query = 'INSERT INTO `message`(`sender_name`, `sender_email`, `message_text`, `is_read`) VALUES (?, ?, ?, ?)';
-
-      connection.query(query, [name, email, message, 0], (err) => {
-        if (err) {
-          console.log(err);
-          res.status(500);
-          res.render('500');
-        } else {
-          res.status(200);
-          res.redirect('/');
-        }
-      });
-    }
-  });
+  knex('messages')
+    .insert(req.body)
+    .then(() => {
+      res.status(200);
+      res.redirect('/');
+    })
+    .catch((err) => {
+      throw Error(err);
+    });
 });
 
 module.exports = router;
