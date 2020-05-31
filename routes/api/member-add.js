1be999db699b480e5673fd3aa9334a488ea08fbf
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2020-05-31 23:01:34 +0430

    Move the `uploader` to a separate module

--- routes/api/member-add.js
+++ routes/api/member-add.js
@@ -1,31 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
-const multer = require('multer');
 const knex = require('../../knex');
 
-const storage = multer.diskStorage({
-  destination(req, file, callback) {
-    callback(null, './uploads/');
-  },
-  filename(req, file, callback) {
-    callback(null, `${new Date().toISOString()}${file.originalname}`);
-  },
-});
-
-const upload = multer({
-  storage,
-  limits: {
-    fileSize: 1024 * 1024,
-  },
-  fileFilter: (req, file, callback) => {
-    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpeg') {
-      callback(null, true);
-    } else {
-      callback(null, false);
-    }
-  },
-});
+const upload = require('../../my_modules/uploader');
 
 router.post('/', upload.single('photo'), (req, res) => {
   const requestData = req.body;
