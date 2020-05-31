80e43dddd0004e6bcd1655c10b19bb1cff556f0f
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2020-05-31 20:02:27 +0430

    Re-write "delete member" controller

--- routes/api/member-delete.js
+++ routes/api/member-delete.js
@@ -1,34 +1,26 @@
 const express = require('express');
 
 const router = express.Router();
-const pool = require('../../connection');
+const knex = require('../../knex');
 
 router.delete('/:id', (req, res) => {
-  const memberId = req.params.id;
+  const { id: memberId } = req.params;
 
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.log(`Error connecting to database: ${err.name}: ${err.message}`);
-      res.status(500);
-      res.render('500');
-    }
-
-    const query = 'DELETE FROM `ex_website`.member WHERE id=?';
-
-    connection.query(query, [memberId], (err, result) => {
-      if (err) {
-        console.log(err);
-        res.status(500);
-        res.render('500');
-      } else {
-        console.log(result);
-        res.status(204);
-        res.end();
-      }
-
-      connection.release();
+  if (!Number(memberId)) {
+    return res.status(400).json({
+      message: 'Invalid parameter',
     });
-  });
+  }
+
+  knex('members')
+    .where('id', memberId)
+    .del()
+    .then(() => (
+      res.status(204).end()
+    ))
+    .catch(() => (
+      res.status(500).render('500')
+    ));
 });
 
 module.exports = router;
