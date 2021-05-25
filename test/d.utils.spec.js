7b1f9d066e878a49cb16ac11e9a78a0c9de14558
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2021-05-26 00:34:49 +0430

    Fix the issue w/ not printing out the generated key in `generateSecretKey`

--- test/d.utils.spec.js
+++ test/d.utils.spec.js
@@ -6,7 +6,7 @@ describe('Utils', () => {
   it('should generate a random string w/ specified length', done => {
     const randomNumber = Math.floor(Math.random() * 100) + 10;
 
-    expect(generateSecretKey(randomNumber, false))
+    expect(generateSecretKey(randomNumber))
       .to.be.a('string')
       .and.with.lengthOf(randomNumber);
 
